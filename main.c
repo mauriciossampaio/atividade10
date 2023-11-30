b758d826e81085ee21f5fa17825980e1cceb7222
mauriciossampaio <141499529+mauriciossampaio@users.noreply.github.com>
2023-11-30 15:51:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 int main() {
-    // Exemplo de uso
-    hash_table_put("chave1", "valor1");
-    hash_table_put("chave2", "valor2");
+    hash_table_put("chave1", "12");
+    hash_table_put("chave2", "24");
 
     printf("Valor para chave1: %s\n", hash_table_get("chave1"));
     printf("Valor para chave2: %s\n", hash_table_get("chave2"));
 
-    printf("A tabela contém chave1? %s\n", hash_table_contains("chave1") ? "Sim" : "Não");
-    printf("A tabela contém chave3? %s\n", hash_table_contains("chave3") ? "Sim" : "Não");
-
+    printf("A tabela contem chave1? %s\n", hash_table_contains("chave1") ? "sim" : "nao");
+    printf("A tabela contem chave3? %s\n", hash_table_contains("chave3") ? "sim" : "nao");
     hash_table_remove("chave1");
-
-    printf("A tabela contém chave1 após a remoção? %s\n", hash_table_contains("chave1") ? "Sim" : "Não");
-
-    liberar_tabela_hash(); // Libera a memória alocada pela tabela hash
+    printf("A tabela contém chave1 após a remoção? %s\n", hash_table_contains("chave1") ? "sim" : "nao");
+    liberar_tabela_hash(); 
 
     return 0;
 }
